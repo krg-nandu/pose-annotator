54a4bfcbab66fc636fb31d6fdafd778801c314af
lakshmi <krg.lakshminarasimhan@gmail.com>
2018-06-13 14:16:02 -0400

    file dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,20 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/**
+    Open a file browser and select the folder containing the images to be annotated
+    @param void
+    @return void
+*/
+void MainWindow::on_actionLoad_folder_triggered()
+{
+    QFileDialog dialog(this);
+    dialog.setDirectory(QDir::homePath());
+    //dialog.setFileMode(QFileDialog::ExistingFile);
+    //dialog.setNameFilter(trUtf8("Splits (*.000 *.001)"));
+    QStringList fileNames;
+    if (dialog.exec())
+        fileNames = dialog.selectedFiles();
 }
